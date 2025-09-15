67ea92bb99b21f05df9f876b2f79adeceb07fa8f
arcplayz <arcplayz@protonmail.ch>
2025-09-15 19:49:29 +0200

    amogus

---
+++ home/arcplayz/.config/pragtical/init.lua
@@ -0,0 +1,48 @@
+local core = require "core"
+
+local config = require "core.config"
+
+config.indent_size = 4
+config.tab_type = "hard"
+config.file_size_limit = 9999999999999
+config.blink_period = 0.25
+
+local syntax = require "core.syntax"
+
+syntax.add {
+	files = { "%.xsh$" },
+	comment = '#',
+	patterns = {},
+	symbols = {},
+}
+
+local lsp = require "plugins.lsp"
+
+lsp.add_server {
+	language = "python",
+	file_patterns = {"%.py$", "%.xsh$"},
+	name = "python-lsp-server",
+	command = {"pylsp"},
+}
+
+lsp.add_server {
+	language = "c/cpp",
+	file_patterns = {"%.c$", "%.h$", "%.cpp$", "%.hpp$"},
+	name = "clangd",
+	command = {"clangd"},
+}
+
+lsp.add_server {
+	language = "meson",
+	file_patterns = {"meson.build"},
+	name = "mesonlsp",
+	command = {"mesonlsp", "--lsp"},
+}
+
+lsp.add_server {
+	language = "bash",
+	file_patterns = {"%.sh"},
+	name = "bash-language-server",
+	command = {"bash-language-server", "start"},
+}
+
