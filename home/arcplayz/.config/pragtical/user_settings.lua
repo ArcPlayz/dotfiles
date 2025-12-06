f81d51d6364e901ed5e86ec58bd4c06d6a860238
arcplayz <arcplayz@protonmail.ch>
2025-12-06 16:26:28 +0100

    16:26:28 06/12/25

---
+++ home/arcplayz/.config/pragtical/user_settings.lua
@@ -0,0 +1,101 @@
+return {
+  ["config"] = {
+    ["animation_rate"] = 1,
+    ["borderless"] = false,
+    ["code_font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "FiraCode Nerd Font Regular",
+          ["path"] = "/usr/share/fonts/TTF/FiraCodeNerdFont-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "grayscale",
+        ["bold"] = false,
+        ["hinting"] = "full",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = true,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["custom_keybindings"] = {
+      ["root:close"] = {
+        [1] = "alt+c"
+      },
+      ["root:switch-to-previous-tab"] = {
+        [1] = "ctrl+`"
+      }
+    },
+    ["disabled_plugins"] = {},
+    ["enabled_plugins"] = {
+      ["lsp"] = true,
+      ["lsp_snippets"] = true,
+      ["macro"] = true,
+      ["projectsearch"] = true,
+      ["reflow"] = true,
+      ["toolbarview"] = true,
+      ["treeview"] = true
+    },
+    ["font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "MonaspiceKr NF Medium",
+          ["path"] = "/usr/share/fonts/OTF/MonaspiceKrNerdFont-Medium.otf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "grayscale",
+        ["bold"] = false,
+        ["hinting"] = "full",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["fps"] = 165,
+    ["ignore_files"] = {},
+    ["line_height"] = 1.3,
+    ["plugins"] = {
+      ["devicons"] = {
+        ["draw_tab_icons"] = true,
+        ["draw_treeview_icons"] = true,
+        ["use_default_chevrons"] = false,
+        ["use_default_dir_icons"] = false
+      },
+      ["drawwhitespace"] = {
+        ["enabled"] = true
+      },
+      ["lsp"] = {
+        ["prettify_json"] = true
+      },
+      ["minimap"] = {
+        ["enabled"] = true
+      },
+      ["terminal"] = {
+        ["arguments"] = {},
+        ["background"] = {
+          [1] = 0,
+          [2] = 0,
+          [3] = 0,
+          [4] = 255
+        },
+        ["shell"] = "/bin/bash",
+        ["term"] = "xterm-256color",
+        ["text"] = {
+          [1] = 255,
+          [2] = 254,
+          [3] = 250,
+          [4] = 255
+        }
+      },
+      ["treeview"] = {
+        ["show_hidden"] = true
+      }
+    },
+    ["theme"] = "bearded-theme-vivid-black"
+  }
+}
